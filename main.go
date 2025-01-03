5d661f6a786b5ec359d3975e22e2e9bf08da6130
Kkhorlane <khorlane@yahoo.com>
2025-01-03 20:52:28 +0000

    Stop copying contents of models folder to Program Files. See change in main.go line 12

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
-  "models"
+  "personweb/models"
   "log"
   "strconv"
   "fmt"
--- make.bat
+++ make.bat
@@ -2,19 +2,15 @@ cd /Projects/PersonWeb
 @cls
 @echo off
 echo.
-echo ****************************
-echo Must be RUN AS ADMINISTRATOR
-echo ****************************
 @echo on
-copy "C:\Projects\PersonWeb\models\*.*" "C:\Program Files\Go\src\models\*.*"
+del personweb.exe
 @if %ERRORLEVEL% == 0 goto :BuildIt
 @echo.
-@echo Copy FAILED.  Exited with status: %errorlevel%
+@echo Delete FAILED.  Exited with status: %errorlevel%
 @echo.
 @pause
 @goto :ExitScript
 :BuildIt
-del personweb.exe
 go build
 :ExitScript
 @echo.
